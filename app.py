14ff196bb3a7caaaed909aff134e19272a34e029
ZainNaveed87 <nzain082@gmail.com>
2025-07-07 18:25:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ from routes import license_routes
 
 app = FastAPI()
 
+# ADD THIS ROUTE
+@app.get("/")
+def root():
+    return {"message": "Secrets License API is Live."}
+
 app.include_router(license_routes.router)
